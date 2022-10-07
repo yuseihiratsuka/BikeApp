a99516c52864bc7df463cc9121f2c88825f96f4e
HiratsukaYusei <yusei@HiratsukaYuseinoMacBook-Pro.local>
2022-10-08 01:39:45 +0900

    home_page_moc

--- lib/view/BasePage_view.dart
+++ lib/view/BasePage_view.dart
@@ -1,12 +1,12 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:testtestest/view/setting_view.dart';
 import 'package:flutter/material.dart';
 
 import 'Home_view.dart';
 import 'Map_view.dart';
 import 'chat_view.dart';
-
 
 class BasePage extends StatefulWidget {
   const BasePage({Key? key}) : super(key: key);
@@ -17,6 +17,36 @@ class BasePage extends StatefulWidget {
 
 class _BasePageState extends State<BasePage> {
   int _selectedIndex = 0;
+  final appbar = <PreferredSizeWidget>[
+    AppBar(
+      title: Text(
+        '',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.black26,
+    ),
+    AppBar(
+      title: Text(
+        'Map',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.black26,
+    ),
+    AppBar(
+      title: Text(
+        'トーク',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.black26,
+    ),
+    AppBar(
+      title: Text(
+        '設定',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.black26,
+    ),
+  ];
   final _pages = <Widget>[
     const HomePage(),
     const MapPage(),
@@ -26,42 +56,57 @@ class _BasePageState extends State<BasePage> {
   void _onTapBottomItem(int value) {
     setState(() {
       _selectedIndex = value;
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black26,
-      appBar:AppBar(title: Text('test',style: TextStyle(color: Colors.white),),backgroundColor: Colors.black26,),
-      body:_pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        currentIndex: _selectedIndex,
-        onTap: _onTapBottomItem,
-        selectedLabelStyle: TextStyle(color: Colors.white),
-        unselectedLabelStyle: TextStyle(color: Colors.red),
-        unselectedItemColor: Colors.white,
-        selectedItemColor: Colors.greenAccent,
-        selectedIconTheme: const IconThemeData(
-          color: Colors.greenAccent,
+    return DefaultTabController(
+      initialIndex: 0, // 最初に表示するタブ
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.black26,
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(text: '参加予定'),
+              Tab(text: '募集一覧'),
+              Tab(text: 'おすすめスポット'),
+            ],
+          ),
         ),
-        unselectedIconTheme: const IconThemeData(
-          color: Colors.white
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          currentIndex: _selectedIndex,
+          onTap: _onTapBottomItem,
+          selectedLabelStyle: TextStyle(color: Colors.white),
+          unselectedLabelStyle: TextStyle(color: Colors.red),
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.greenAccent,
+          selectedIconTheme: const IconThemeData(
+            color: Colors.greenAccent,
+          ),
+          unselectedIconTheme: const IconThemeData(color: Colors.white),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                ),
+                label: 'ホーム'),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.pink,
+                icon: Icon(Icons.pin_drop_outlined),
+                label: '近くのライダー'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.message_outlined), label: 'トーク'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined), label: "マイページ"),
+            //BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
+          ],
+          type: BottomNavigationBarType.fixed,
         ),
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined,), label: 'ホーム'),
-          BottomNavigationBarItem(
-            backgroundColor: Colors.pink,
-              icon: Icon(Icons.pin_drop_outlined), label: '近くのライダー'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.message_outlined), label: 'チャット'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "マイページ"),
-          //BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
-        ],
-        type:BottomNavigationBarType.fixed,
       ),
     );
   }
--- lib/view/Home_view.dart
+++ lib/view/Home_view.dart
@@ -10,6 +10,105 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home',style: TextStyle(color: Colors.white)));
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Container(
+                alignment: Alignment.topLeft,
+                child: Text('ツーリング予定',
+                    style: TextStyle(color: Colors.white, fontSize: 25))),
+          ),
+          Stack(children: [
+            Opacity(opacity: 0.4, child: Image.asset('images/img.png')),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: Text('参加予定のツーリングはありません',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Align(
+              widthFactor: 100,
+              heightFactor: 4,
+              child: ElevatedButton(
+                child: const Text('参加する',
+                    style: TextStyle(color: Colors.white, fontSize: 15)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[700],
+                  onPrimary: Colors.black,
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ]),
+          Container(
+              alignment: Alignment.topLeft,
+              child: Text('イベント予定',
+                  style: TextStyle(color: Colors.white, fontSize: 25))),
+          Stack(children: [
+            Opacity(opacity: 0.4, child: Image.asset('images/img_1.png')),
+            Padding(
+              padding: const EdgeInsets.only(top: 65.0),
+              child: Center(
+                child: Text('参加予定のイベントはありません',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Align(
+              widthFactor: 100,
+              heightFactor: 6,
+              child: ElevatedButton(
+                child: const Text('参加する',
+                    style: TextStyle(color: Colors.white, fontSize: 15)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[700],
+                  onPrimary: Colors.black,
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ]),
+          Container(
+              alignment: Alignment.topLeft,
+              child: Text('おすすめスポット',
+                  style: TextStyle(color: Colors.white, fontSize: 25))),
+          Stack(children: [
+            Opacity(opacity: 0.4, child: Image.asset('images/img_2.png')),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: Text('参加予定のツーリングはありません',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Align(
+              widthFactor: 100,
+              heightFactor: 4,
+              child: ElevatedButton(
+                child: const Text('参加する',
+                    style: TextStyle(color: Colors.white, fontSize: 15)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[700],
+                  onPrimary: Colors.black,
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ]),
+        ],
+      ),
+    );
   }
 }
