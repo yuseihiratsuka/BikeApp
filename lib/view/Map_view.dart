2d058c6e047fb5bea8c00b7e54789dd985335505
HiratsukaYusei <yusei@HiratsukaYuseinoMacBook-Pro.local>
2022-10-21 00:17:53 +0900

    add currentLocation

--- lib/view/Map_view.dart
+++ lib/view/Map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -12,6 +13,20 @@ class _MapPageState extends State<MapPage> {
   late GoogleMapController _controller;
   static const CameraPosition _initialPosition = CameraPosition(
       target: LatLng(34.654279184495486, 135.0028980062489), zoom: 8);
+  late final CameraPosition currentPosition;
+  Future<void> getCurrentPosition() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('取得失敗');
+      }
+    }
+    final Position _currentPosition = await Geolocator.getCurrentPosition();
+    currentPosition = CameraPosition(
+        target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+        zoom: 16);
+  }
 
   final Set<Marker> _markers = {
     const Marker(
@@ -28,9 +43,22 @@ class _MapPageState extends State<MapPage> {
     return GoogleMap(
       markers: _markers,
       initialCameraPosition: _initialPosition,
-      onMapCreated: (GoogleMapController controller) {
+      onMapCreated: (GoogleMapController controller) async {
+        await getCurrentPosition();
         _controller = controller;
+        setState(() {
+          _markers.add(
+            Marker(
+                markerId: const MarkerId('3'),
+                position: currentPosition.target,
+                infoWindow:
+                    const InfoWindow(title: '現在地', snippet: 'これは現在地です')),
+          );
+        });
+        _controller
+            .animateCamera(CameraUpdate.newCameraPosition(currentPosition));
       },
+      myLocationEnabled: true, //現在地のボタン
     );
   }
 }
